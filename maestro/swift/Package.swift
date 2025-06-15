2e0f60113b27c38f916180825ffc0a51e4e0ea68
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-15 03:59:59 -0300

    Replace socket server with SwiftNIO

--- maestro/swift/Package.swift
+++ maestro/swift/Package.swift
@@ -5,10 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "maestro",
+    platforms: [
+        .macOS(.v12)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.64.0")
+    ],
     targets: [
         // Executable containing the HTTP server and orchestration logic.
         .executableTarget(
             name: "maestro",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio")
+            ],
             path: "Sources"
         ),
         // Unit tests exercising the orchestration logic.
--- maestro/swift/Sources/Core/StartServer.swift
+++ maestro/swift/Sources/Core/StartServer.swift
@@ -1,67 +1,62 @@
 import Foundation
-#if os(Linux)
-import Glibc
-#else
-import Darwin
-#endif
+import NIO
+import NIOHTTP1
 
-/// Minimal HTTP server handling GET requests from Home Assistant.
-func startServer(on port: Int32, maestro: Maestro) throws {
-#if os(Linux)
-    let serverFD = socket(AF_INET, Int32(SOCK_STREAM.rawValue), 0)
-#else
-    let serverFD = socket(AF_INET, SOCK_STREAM, 0)
-#endif
-    guard serverFD >= 0 else { fatalError("Unable to create socket") }
+final class HTTPHandler: ChannelInboundHandler {
+    typealias InboundIn = HTTPServerRequestPart
+    typealias OutboundOut = HTTPServerResponsePart
 
-    var value: Int32 = 1
-    setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR, &value, socklen_t(MemoryLayout<Int32>.size))
+    private let maestro: Maestro
+    private var uri: String = "/"
 
-    var addr = sockaddr_in()
-    addr.sin_family = sa_family_t(AF_INET)
-    addr.sin_port = in_port_t(port).bigEndian
-    addr.sin_addr = in_addr(s_addr: INADDR_ANY.bigEndian)
-
-    var bindAddr = sockaddr()
-    memcpy(&bindAddr, &addr, MemoryLayout<sockaddr_in>.size)
-    guard bind(serverFD, &bindAddr, socklen_t(MemoryLayout<sockaddr_in>.size)) >= 0 else {
-        fatalError("bind failed")
+    init(maestro: Maestro) {
+        self.maestro = maestro
     }
-    listen(serverFD, 10)
-    print("Server listening on port \(port)")
 
-    while true {
-        var clientAddr = sockaddr()
-        var len: socklen_t = socklen_t(MemoryLayout<sockaddr>.size)
-        let clientFD = accept(serverFD, &clientAddr, &len)
-        if clientFD < 0 { continue }
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        let part = unwrapInboundIn(data)
+        switch part {
+        case .head(let head):
+            uri = head.uri
+        case .end:
+            respond(context: context)
+        case .body:
+            break
+        }
+    }
 
-        var buffer = [UInt8](repeating: 0, count: 1024)
-        let count = read(clientFD, &buffer, 1024)
-
-        var statusLine = "HTTP/1.1 200 OK"
+    private func respond(context: ChannelHandlerContext) {
+        var head = HTTPResponseHead(version: .http1_1, status: .ok)
         var body = "OK"
+        if uri == "/run" {
+            maestro.run()
+        } else {
+            head.status = .notFound
+            body = "Not Found"
+        }
+        head.headers.add(name: "Content-Length", value: "\(body.utf8.count)")
+        context.write(self.wrapOutboundOut(.head(head)), promise: nil)
+        var buffer = context.channel.allocator.buffer(string: body)
+        context.write(self.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: nil)
+        context.writeAndFlush(self.wrapOutboundOut(.end(nil)), promise: nil)
+    }
+}
 
-        if count > 0 {
-            let request = String(decoding: buffer[0..<count], as: UTF8.self)
-            if request.hasPrefix("GET ") {
-                if let firstLine = request.components(separatedBy: "\r\n").first,
-                   let range = firstLine.range(of: " ") {
-                    let start = firstLine.index(after: range.lowerBound)
-                    let end = firstLine.range(of: " ", range: start..<firstLine.endIndex)?.lowerBound ?? firstLine.endIndex
-                    let path = firstLine[start..<end]
-                    if path == "/run" {
-                        maestro.run()
-                    } else {
-                        statusLine = "HTTP/1.1 404 Not Found"
-                        body = "Not Found"
-                    }
-                }
+/// HTTP server built with SwiftNIO.
+func startServer(on port: Int32, maestro: Maestro) throws {
+    let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+    let bootstrap = ServerBootstrap(group: group)
+        .serverChannelOption(ChannelOptions.backlog, value: 256)
+        .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+        .childChannelInitializer { channel in
+            channel.pipeline.configureHTTPServerPipeline().flatMap {
+                channel.pipeline.addHandler(HTTPHandler(maestro: maestro))
             }
         }
+        .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
 
-        let response = "\(statusLine)\r\nContent-Length: \(body.utf8.count)\r\n\r\n\(body)"
-        _ = response.withCString { send(clientFD, $0, strlen($0), 0) }
-        close(clientFD)
-    }
+    let channel = try bootstrap.bind(host: "0.0.0.0", port: Int(port)).wait()
+    print("Server listening on port \(port)")
+    try channel.closeFuture.wait()
+    try group.syncShutdownGracefully()
 }
