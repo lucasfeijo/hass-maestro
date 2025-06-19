acf9cdcea115797c24120e6a4753b52752a3657e
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-18 22:49:52 -0300

    fix socket

--- maestro/swift/Sources/Core/StartServer.swift
+++ maestro/swift/Sources/Core/StartServer.swift
@@ -13,7 +13,7 @@ import ucrt
 /// Minimal HTTP server handling GET requests from Home Assistant.
 func startServer(on port: Int32, maestro: Maestro) throws {
     #if os(Linux)
-    let serverFD = socket(AF_INET, Int32(SOCK_STREAM.rawValue), 0)
+    let serverFD = socket(AF_INET, SOCK_STREAM, 0)
     #else
     let serverFD = socket(AF_INET, Int32(SOCK_STREAM), 0)
     #endif
