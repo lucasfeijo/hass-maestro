0084e65daca62809cb93afa39aae233d37d6818c
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-20 17:03:05 -0300

    fix description

--- maestro/swift/Sources/Context/StateContext+Description.swift
+++ maestro/swift/Sources/Context/StateContext+Description.swift
@@ -3,7 +3,10 @@ import Foundation
 extension StateContext: CustomStringConvertible {
     public var description: String {
         let sceneName = String(describing: scene)
-        let progress = String(format: "%.2f", environment.sunsetProgress)
-        return "scene: \(sceneName) sunset: \(progress)"
+        return """
+        autoMode: \(environment.autoMode)
+        scene: \(sceneName)
+        timeOfDay: \(environment.timeOfDay)
+        """
     }
 }
