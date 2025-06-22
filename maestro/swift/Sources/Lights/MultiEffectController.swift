910e01d264e8038f02f215c9a77997515bc40b76
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-22 02:18:37 -0300

    revert multi

--- maestro/swift/Sources/Lights/MultiEffectController.swift
+++ maestro/swift/Sources/Lights/MultiEffectController.swift
@@ -9,38 +9,20 @@ public final class MultiEffectController: EffectController {
     }
 
     public func setLightState(state: LightState) {
-        let group = DispatchGroup()
         for controller in controllers {
-            group.enter()
-            DispatchQueue.global().async {
-                controller.setLightState(state: state)
-                group.leave()
-            }
+            controller.setLightState(state: state)
         }
-        group.wait()
     }
 
     public func stopAllDynamicScenes() {
-        let group = DispatchGroup()
         for controller in controllers {
-            group.enter()
-            DispatchQueue.global().async {
-                controller.stopAllDynamicScenes()
-                group.leave()
-            }
+            controller.stopAllDynamicScenes()
         }
-        group.wait()
     }
 
     public func setInputBoolean(entityId: String, to state: Bool) {
-        let group = DispatchGroup()
         for controller in controllers {
-            group.enter()
-            DispatchQueue.global().async {
-                controller.setInputBoolean(entityId: entityId, to: state)
-                group.leave()
-            }
+            controller.setInputBoolean(entityId: entityId, to: state)
         }
-        group.wait()
     }
 }
