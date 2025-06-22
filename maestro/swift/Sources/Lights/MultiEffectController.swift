31b8efd2098676b23d756652807dda63ef74505c
Lucas Feijo <lucas.feijo@gmail.com>
2025-06-22 02:16:55 -0300

    Document concurrent side effect execution

--- maestro/swift/Sources/Lights/MultiEffectController.swift
+++ maestro/swift/Sources/Lights/MultiEffectController.swift
@@ -9,20 +9,38 @@ public final class MultiEffectController: EffectController {
     }
 
     public func setLightState(state: LightState) {
+        let group = DispatchGroup()
         for controller in controllers {
-            controller.setLightState(state: state)
+            group.enter()
+            DispatchQueue.global().async {
+                controller.setLightState(state: state)
+                group.leave()
+            }
         }
+        group.wait()
     }
 
     public func stopAllDynamicScenes() {
+        let group = DispatchGroup()
         for controller in controllers {
-            controller.stopAllDynamicScenes()
+            group.enter()
+            DispatchQueue.global().async {
+                controller.stopAllDynamicScenes()
+                group.leave()
+            }
         }
+        group.wait()
     }
 
     public func setInputBoolean(entityId: String, to state: Bool) {
+        let group = DispatchGroup()
         for controller in controllers {
-            controller.setInputBoolean(entityId: entityId, to: state)
+            group.enter()
+            DispatchQueue.global().async {
+                controller.setInputBoolean(entityId: entityId, to: state)
+                group.leave()
+            }
         }
+        group.wait()
     }
 }
